76e1476692e46e604d30282e86c26f2e6432ac67
Shivakumara Narayanaswamy <Shivakumara_Narayanaswamy@intuit.com>
2015-02-13 11:51:30 +0530

    -Adding booking details

--- express.js
+++ express.js
@@ -32,6 +32,9 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res){
     var txtWebMsg = req.param('txtweb-message');
     var phoneHash = req.param('txtweb-mobile');
+
+   
+
     if(/reg .*/g.test(txtWebMsg)){
         var user = extractUserFromMsg(txtWebMsg);
         createUser(user, phoneHash, function(err, result){
@@ -44,8 +47,27 @@ app.get('/', function(req, res){
         });
     }
     else if (/req .*/g.test(txtWebMsg)){
-        
-        //todo: process booking request
+        getUserDetails(phoneHash, function(err, rows){
+
+            if(err) {
+                res.render('error_message');
+            } else {
+                if(rows.length > 0){
+                    var user = rows[0];
+                    var bookingDeatils = extractBookingDetails(txtWebMsg);
+                    bookCab(user, bookingDeatils, function(err, data){
+                        if(err){
+                            console.log(err)
+                            res.render('error_message')
+                        } else {
+                            res.render('confirmation', {name:data.name, phone:data.phone, rno:data.rno, from:data.from, to:data.to, date:data.date, time:data.time})
+                        }
+                    });
+                } else {
+                    res.render('registration');    
+                }
+            }
+        });
     }
     else {
         checkUserExists(phoneHash, function(err, rows){
@@ -80,6 +102,18 @@ app.get('/', function(req, res){
     }
 });
 
+function bookCab(user, bookingDetils, cb) {
+    
+    cb(false, {name:user.name, phone:user.phone, from:getLocalityFromPin(bookingDetils.fromPin), 
+        to:getLocalityFromPin(bookingDetils.toPin), date:bookingDetils.date, time:bookingDetils.time, rno:"00002345ka456"})
+}
+
+function getUserDetails(phoneHash, cb) {
+    connection.query('SELECT * from users where phone_hash="' + phoneHash + '"', function(err, rows) {
+        cb(err, rows)
+    });
+}
+
 function savePhoneHash(phoneHash, cb) {
     var post  = {phone_hash: phoneHash, reg_req:0 };
     var query = connection.query('INSERT INTO users SET ?', post, function(err, result) {
@@ -95,7 +129,6 @@ function checkUserRegistered(phoneHash, cb) {
 }
 
 function checkUserExists(phoneHash, cb){
-    var b = false;
     connection.query('SELECT count(*) as count from users where phone_hash="' + phoneHash + '"', function(err, rows) {
            cb(err, rows)
     });
@@ -103,20 +136,31 @@ function checkUserExists(phoneHash, cb){
 
 function createUser(user, phoneHash, cb){
     console.log("Saving user :" + user.name + user.phone + user.email)
-    var query = connection.query('Update users SET name = ?, phone_no = ?, email = ? where phone_hash = ?', [user.name, user.phone, user.email, phoneHash], function(err, result) {
+    var query = connection.query('Update users SET name = ?, phone_no = ?, email = ? where phone_hash = ?', [user.name, user.phone, user.email, phoneHash], 
+        function(err, result) {
        cb(err, result);
     });
     console.log(query.sql);
 }
 
 function getLocalityFromPin(pinCode){
-
+    if(pinCode == "500022")
+        return "Yeshwanthpura";
+    else if(pinCode == "500103")
+        return "Bellandur";
 }
 
 function extractUserFromMsg(txtWebMsg) {
     console.log("received message:" + txtWebMsg);
     var data = /reg email:(.*)\s+phone:(.*)\s+name:(.*)/g.exec(txtWebMsg);
     return {email:data[1], phone:data[2], name:data[3]}
+}
+
+function extractBookingDetails(txtWebMsg) {
+    console.log("received message:" + txtWebMsg);
+    //req frmPin:500022 toPin:5000103 frmLndmrk:Metro date(dd/mm/yy):22/02/14 time:3am
+    var data = /req frmPin:(.*)\s+toPin:(.*)\s+frmLndmrk:(.*)\s+date\(dd\/mm\/yy\):(.*)\s+time:(.*)/g.exec(txtWebMsg);
+    return {fromPin:data[1], toPin:data[2], fromLandmark:data[3], date:data[4], time:data[5]}
 }
 
 app.listen(8080);
